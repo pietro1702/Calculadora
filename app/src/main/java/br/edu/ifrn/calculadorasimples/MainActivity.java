848d8e6f664296a3e75632c083b060d368ce42bb
Pietro Kauê <pietrocelestino1702@gmail.com>
2017-11-29 14:48:54 -0300

    Testa aí, txio

--- app/src/main/java/br/edu/ifrn/calculadorasimples/MainActivity.java
+++ app/src/main/java/br/edu/ifrn/calculadorasimples/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     Button btDiv;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
